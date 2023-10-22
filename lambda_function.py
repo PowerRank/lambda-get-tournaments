f8b964c5a219dffd1d441198865eeec0243a8a08
aahm2221 <aisha.ahmed1@yahoo.com>
2023-10-22 13:46:30 -0500

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,18 @@
+import boto3
 import json
+import os
+from boto3.dynamodb.conditions import Key
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ['TABLE_NAME'])
 
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda 2!')
-    }
-
+    response = table.query(
+        ProjectionExpression='TournamentId,#n',
+        KeyConditionExpression=Key('PK').eq('Tournament'),
+        ExpressionAttributeNames = {'#n': 'Name'}
+    )
+    tournaments = response['Items']
+    for tournament in tournaments:
+        tournament['tournamentId']=int(tournament['tournamentId'])
+    return {'statusCode': 200, 'body':json.dumps(tournaments)}
